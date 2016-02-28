d4283929ee4e31f9777a37080b7a7541f336d199
accraze <accraze@gmail.com>
2016-02-28 09:53:17 -0800

    refactor(licenses): move licenses to their own modules

--- src/make-license.js
+++ src/make-license.js
@@ -1,9 +1,13 @@
 var fs = require('fs');
 var inquirer = require("inquirer");
+var mit = require("./licenses/mit")
+var isc = require("./licenses/isc")
+var unlicense = require("./licenses/unlicense")
 
 module.exports = makeLicense
 
 function makeLicense (args) {
+
   if (args.license === 'MIT') {
     var questions = copyrightQuestions();
     inquirer.prompt(questions, function(answers){
@@ -82,59 +86,3 @@ function print(license, filename) {
       stream.end();
     });
 };
-
-var mit = "\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\n"+
-"of this software and associated documentation files (the \"Software\"), to deal\n"+
-"in the Software without restriction, including without limitation the rights\n"+
-"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"+
-"copies of the Software, and to permit persons to whom the Software is\n"+
-"furnished to do so, subject to the following conditions:\n"+
-"\n"+
-"The above copyright notice and this permission notice shall be included in all\n"+
-"copies or substantial portions of the Software.\n"+
-"\n"+
-"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"+
-"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"+
-"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"+
-"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"+
-"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"+
-"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"+
-"SOFTWARE.";
-
-var isc = " \n\n"+
-"Permission to use, copy, modify, and/or distribute this software for any\n"+
-"purpose with or without fee is hereby granted, provided that the above\n"+
-"copyright notice and this permission notice appear in all copies.\n"+
-"\n"+
-"THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES\n"+
-"WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF\n"+
-"MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR\n"+
-"ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES\n"+
-"WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN\n"+
-"ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF\n"+
-"OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.\n";
-
-var unlicense = "This is free and unencumbered software released into the public domain.\n"+
-"\n"+
-    "Anyone is free to copy, modify, publish, use, compile, sell, or\n"+
-    "distribute this software, either in source code form or as a compiled\n"+
-    "binary, for any purpose, commercial or non-commercial, and by any\n" +
-    "means.\n"+
-"\n" +
-    "In jurisdictions that recognize copyright laws, the author or authors\n" +
-    "of this software dedicate any and all copyright interest in the\n" +
-    "software to the public domain. We make this dedication for the benefit\n" +
-    "of the public at large and to the detriment of our heirs and\n"+
-    "successors. We intend this dedication to be an overt act of\n"+
-    "relinquishment in perpetuity of all present and future rights to this\n"+
-    "software under copyright law.\n"+
-"\n"+
-    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,\n"+
-    "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n"+
-    "MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n"+
-    "IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR\n"+
-    "OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n"+
-    "ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n"+
-    "OTHER DEALINGS IN THE SOFTWARE.\n"+
-"\n"+
-    "For more information, please refer to <http://unlicense.org>";
