cdf1e0bde51f0f0a642f54131b7b2f8ec53e7f64
rista404 <risticn6@gmail.com>
2016-10-02 03:14:39 +0200

    Adds build script before test script in travis.yml

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,11 +2,13 @@ var expect = require('chai').expect
 var makeLicense = require('../dist/make-license.js').default
 
 describe('makeLicense', function () {
+	
   it('should raise error when no match for license', function (done) {
     var args = {license: 'blahblahblah'};
     expect(makeLicense.bind(makeLicense,args)).to.throw(Error);
     done();
   });
+
   it('should exist', function (done) {
     expect(makeLicense).to.exist;
     done();
